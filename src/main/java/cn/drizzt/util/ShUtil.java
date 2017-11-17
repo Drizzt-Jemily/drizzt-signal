46c4dad22da79f37db34cda3f1d6d54cfcdf3d05
Drizzt <Drizzt@Drizzt-PC>
2017-11-17 10:02:53 +0800

    加入jna配置

---
+++ src/main/java/cn/drizzt/util/ShUtil.java
@@ -0,0 +1,38 @@
+package cn.drizzt.util;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+public interface ShUtil extends Library {
+
+    ShUtil INSTANCE = (ShUtil) Native.loadLibrary(Const.CTI_SO_PATH, ShUtil.class);
+
+    public int SsmStartCti(String shconfig, String shindex);
+
+    public int SsmCloseCti();
+
+    public String SsmGetLastErrMsgA();
+
+    public int SsmGetMaxCh();
+
+    public int SsmGetChState(int ch);
+
+    public int SsmSetTxCallerId(int ch, String phNum);
+
+    public int SsmAutoDial(int ch, String phNum);
+
+    public int SsmChkAutoDial(int ch);
+
+    public int SsmHangup(int ch);
+
+    public int SsmRecToFile(int ch, String fileName, int nFormat, int dwStartPos, int dwBytes, int dwTime, int nMask);
+
+    public int SsmStopRecToFile(int ch);
+
+    public int SsmChkRecToFile(int ch);
+
+    public int SsmGetToneAnalyzeResult(int ch);
+    
+    public int SsmDetectBargeIn(int ch);
+
+}
