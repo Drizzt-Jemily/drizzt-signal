eeb7c038e73daff96089e89cd1f830083021068f
Drizzt <Drizzt@673W2JAHHIH8J1N>
2018-10-02 21:06:11 +0800

    呼叫超时改成1

--- src/main/java/cn/drizzt/thread/DialDispatcher.java
+++ src/main/java/cn/drizzt/thread/DialDispatcher.java
@@ -192,7 +192,7 @@ public class DialDispatcher implements Runnable {
 
 				if (b) { // 呼叫超时，暂时按正常处理
 					// chManager.setCallResult(Const.CALL_RESULT_98);
-					signalAuth.setCallResult(Const.CALL_RESULT_97);
+					signalAuth.setCallResult(Const.CALL_RESULT_1);
 				}
 
 				// 如果结果未变化，进行语音识别
